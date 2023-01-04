264aeb3f83bfb3a39ba6f583a9a126b2ffbe6e7d
himanshu_red <himanshutdw98@gmail.com>
2023-01-04 15:18:21 +0530

    comment entity class added

---
+++ src/main/java/com/reddit/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.reddit.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="comment")
+    private String comment;
+    @CreationTimestamp
+    @Column(name="created_at", updatable = false, nullable = false)
+    private Timestamp createdAt;
+    @UpdateTimestamp
+    @Column(name="updated_at", nullable = false)
+    private Timestamp updatedAt;
+}
