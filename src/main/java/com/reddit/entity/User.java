d27cc4289f8613152ee4c642fa717c44b33727c5
Harsha Gandikota <gandikota.wardhan.22@mountblue.tech>
2023-01-04 15:06:26 +0530

    Add User entity

---
+++ src/main/java/com/reddit/entity/User.java
@@ -0,0 +1,28 @@
+package com.reddit.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+    @Column(name="created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+}
