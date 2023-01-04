ec7cc29e70cea0936641e8be7d469d25693702c0
Harsha Gandikota <gandikota.wardhan.22@mountblue.tech>
2023-01-04 20:18:43 +0530

    Add controller methods to add, update and delete comments

--- src/main/java/com/reddit/service/CommentService.java
+++ src/main/java/com/reddit/service/CommentService.java
@@ -1,11 +1,41 @@
 package com.reddit.service;
 
+import com.reddit.entity.Comment;
+import com.reddit.entity.Post;
 import com.reddit.repository.CommentRepository;
+import com.reddit.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CommentService {
     @Autowired
+    private PostRepository postRepository;
     private CommentRepository commentRepository;
+
+    public void addComment(String postId, Comment comment){
+        Optional<Post> postOptional = postRepository.findById(Long.valueOf(Integer.parseInt(postId)));
+        if(postOptional.isPresent()){
+            Post post = postOptional.get();
+            comment.setPost(post);
+            commentRepository.save(comment);
+        }
+    }
+    public Comment getComment(String commentId){
+        Optional<Comment> commentOptional = commentRepository.findById(Long.valueOf(Integer.parseInt(commentId)));
+        if(commentOptional.isPresent()){
+            Comment comment = commentOptional.get();
+            return comment;
+        }
+        return null;
+    }
+    public void deleteComment(String commentId){
+        Comment comment = getComment(commentId);
+        if(comment!=null){
+            comment.setPost(null);
+            commentRepository.delete(comment);
+        }
+    }
 }
