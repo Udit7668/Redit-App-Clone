2d65825dd7613f7a57e641371e231a53758cb38c
Udit7668 <294uditmishra@gmail.com>
2023-01-04 16:09:58 +0530

    controller added of comment and user and added thymeleaf dependency and test it

--- src/main/java/com/reddit/controller/PostController.java
+++ src/main/java/com/reddit/controller/PostController.java
@@ -1,8 +1,13 @@
 package com.reddit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PostController {
 
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 }
---
+++ src/main/java/com/reddit/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.reddit.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
