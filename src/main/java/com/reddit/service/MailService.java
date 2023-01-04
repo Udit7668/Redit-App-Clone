0abf6bc50d229e84533ee0ec1c804e0607a39076
Udit7668 <294uditmishra@gmail.com>
2023-01-04 23:15:59 +0530

    make email request as asynchronous request

--- src/main/java/com/reddit/service/MailService.java
+++ src/main/java/com/reddit/service/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,7 @@ public class MailService {
     private JavaMailSender javaMailSender;
     @Autowired
     private MailContentBuilder mailContentBuilder;
+    @Async
     public void sendEmail(NotificationEmail notificationEmail){
         MimeMessagePreparator messagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
