efb0e0c09663f95ba74f792061c59e543865cc97
Harsha Gandikota <gandikota.wardhan.22@mountblue.tech>
2023-01-06 12:43:13 +0530

    Add option to create and delete subreddits

--- src/main/java/com/reddit/service/SubredditService.java
+++ src/main/java/com/reddit/service/SubredditService.java
@@ -1,28 +1,36 @@
 package com.reddit.service;
 
 import com.reddit.entity.Subreddit;
+import com.reddit.entity.User;
 import com.reddit.repository.PostRepository;
 import com.reddit.repository.SubredditRepository;
+import com.reddit.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 
 @Service
 public class SubredditService {
     @Autowired
     private SubredditRepository subredditRepository;
+    @Autowired
+    private UserRepository userRespository;
 
-
-    public void createSubreddit(Subreddit subreddit, Long subredditId) {
-       subredditRepository.save(subreddit);
+    public void createSubreddit(Subreddit subreddit, Long subredditId, String username) {
+        subreddit.setCreatedDate(Timestamp.from(Instant.now()));
+        User user = userRespository.findByUsername(username).get();
+        subreddit.getAdmins().add(user);
+        subredditRepository.save(subreddit);
     }
-
     public List<Subreddit> findAll() {
-       return subredditRepository.findAll();
+        return subredditRepository.findAll();
     }
-
-    public Subreddit getSubredditById(long parseLong) {
-        return null;
+    public void deleteSubredditById(Long subredditId) {
+        subredditRepository.deleteById(subredditId);
     }
-}
+    public Subreddit findById(Long subredditId) {
+        return subredditRepository.findById(subredditId).get();
+    }}
