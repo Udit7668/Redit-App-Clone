70528e8978a9319f7dd23a951cb4ba7a1d0404a9
Harsha Gandikota <gandikota.wardhan.22@mountblue.tech>
2023-01-06 12:43:13 +0530

    Add option to create and delete subreddits

--- src/main/java/com/reddit/controller/SubredditController.java
+++ src/main/java/com/reddit/controller/SubredditController.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import com.reddit.entity.Subreddit;
 import com.reddit.service.SubredditService;
@@ -16,15 +14,36 @@ public class SubredditController {
     @Autowired
     private SubredditService subredditService;
 
-    @PostMapping("/addSubreddit")
-    public String createSubreddit(@ModelAttribute("subreddit")Subreddit subreddit, @RequestParam(value = "subredditId",required = false) Long subredditId){
-        subredditService.createSubreddit(subreddit,subredditId);
-       return "";
+    @GetMapping("/home/add")
+    public String create(Model model){
+        model.addAttribute("subreddit",new Subreddit());
+        return "saveSubreddit";
     }
-    @GetMapping("/showSubreddit")
-    public List<Subreddit> showSubreddit(){
-       return subredditService.findAll();
-
+    @PostMapping("/home/addSubreddit")
+    public String createSubreddit(@ModelAttribute("subreddit")Subreddit subreddit,
+                                  @RequestParam(name="username") String username,
+                                  @RequestParam(value = "subredditId",
+                                          required = false) Long subredditId){
+        subredditService.createSubreddit(subreddit,subredditId,username);
+        return "redirect:/home/";
+    }
+    @CrossOrigin
+    @GetMapping("/home/showSubreddit")
+    public String showSubreddit(Model model){
+        List<Subreddit> subredditList= subredditService.findAll();
+        System.out.println(subredditList);
+        model.addAttribute("allSubreddit",subredditList);
+        return "showsubreddit";
+    }
+    @GetMapping("/home/deleteSubreddit")
+    public String deleteSubreddit(@RequestParam("subredditId") Long subredditId){
+        subredditService.deleteSubredditById(subredditId);
+        return "showsubreddit";
+    }
+    @GetMapping("/home/showSubredditId")
+    public Subreddit showSubredditById(@RequestParam("subredditId")Long subredditId){
+        Subreddit subreddit =subredditService.findById(subredditId);
+        return  subreddit;
     }
 
 }
