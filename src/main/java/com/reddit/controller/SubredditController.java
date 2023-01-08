05c8158e2215a3f654c4dbcfd1b0ec4f3fc45c52
Udit7668 <294uditmishra@gmail.com>
2023-01-08 12:45:26 +0530

    added upvote feature on subreddit page

--- src/main/java/com/reddit/controller/SubredditController.java
+++ src/main/java/com/reddit/controller/SubredditController.java
@@ -117,5 +117,19 @@ public class SubredditController {
     model.addAttribute("thesubreddit", subreddit);
     return "subreddit-post";
    }
+
+
+   @GetMapping("/upvote")
+   public String upVote(@RequestParam("postId") Long postId, @RequestParam("username") String username,Model model,@RequestParam("name") String name ){
+       System.out.println(">> upvoting : " + postId + " " + username);
+       this.postService.upvotePost(postId, username); 
+       List<Post> posts=this.postService.sortPost(name);
+       Subreddit subreddit=this.subredditService.findByName(name);
+       List<Subreddit> subreddits = this.subredditService.findAll();
+       model.addAttribute("posts", posts);
+       model.addAttribute("subreddits", subreddits);
+       model.addAttribute("thesubreddit", subreddit);
+       return "subreddit-post";
+   }
      
 }
