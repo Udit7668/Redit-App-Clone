27f1cb7f23139b49eebccc2fe85c6da3fdcd8c8e
Harsha Gandikota <gandikota.wardhan.22@mountblue.tech>
2023-01-10 16:23:13 +0530

    Add security in the subreddit page for deleting the subreddit

--- src/main/java/com/reddit/controller/SubredditController.java
+++ src/main/java/com/reddit/controller/SubredditController.java
@@ -110,6 +110,12 @@ public class SubredditController {
                 break;
             }
         }
+        for (User subredditMods: subreddit.getAdmins()) {
+            if(subredditMods.getUsername().equals(authentication.getName())){
+                model.addAttribute("mod", "yes");
+                break;
+            }
+        }
         System.out.println(">> subreddit name : " + subreddit.getName());
         return "subreddit-post";
     }
